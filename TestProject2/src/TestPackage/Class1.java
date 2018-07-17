2a968da75f0d96c50149db3a19a9c45da94a1ebc
Suman <suman.m.mailaarsetty@oracle.com>
2018-07-17 20:29:20 +0530

    aaa

---
+++ TestProject2/src/TestPackage/Class1.java
@@ -0,0 +1,17 @@
+package TestPackage;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+  @Test
+  public void f() {
+	  
+	  System.out.println("suman");
+  }
+  
+  @Test
+  public void g() {
+	  
+	  System.out.println("verma");
+  }
+}
