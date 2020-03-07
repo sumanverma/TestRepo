d3a9b6e4bdd7bf5ef3ed075cfe457c0c0beeabf7
Suman <suman.m.mailaarsetty@oracle.com>
2020-03-07 20:38:14 +0530

    added new test case

---
+++ TestProject2/src/TestPackage/Class4.java
@@ -0,0 +1,17 @@
+package TestPackage;
+
+import org.testng.annotations.Test;
+
+public class Class2 {
+  @Test
+  public void f() {
+	  
+	  System.out.println("xyz");
+  }
+  
+  @Test
+  public void g() {
+	  
+	  System.out.println("harry");
+  }
+}
